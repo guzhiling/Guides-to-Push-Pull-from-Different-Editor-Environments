d7eb679dbab2c3743279636e3a371a906103b3d6
gzl <gzlcuhk@gmail.com>
2018-08-01 10:48:15 +0800

    add push method

--- ver0.R
+++ ver0.R
@@ -1,9 +1,12 @@
-# this is a trial to pull and push using rstudio 
+# this is a trial to pull, commit and push using rstudio 
 
 # PULL
 # step1: file -> new project -> from git -> copy url of your interested repository online
 # step2: select the file from Files in the cloned project folder
 
 
+# COMMIT
+# step1: Tools -> version control -> commit (add your comment / descruiption of the adjustment)
+
 # PUSH 
-# 
+# step1: Tools -> version control -> push (log in by username and password)
