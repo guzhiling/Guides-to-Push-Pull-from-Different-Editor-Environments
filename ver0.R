5aabbd7d8db256bc9083e1b23088592b73db4e8c
gzl <gzlcuhk@gmail.com>
2018-08-01 10:45:33 +0800

    add pull methods

--- ver0.R
+++ ver0.R
@@ -1,4 +1,9 @@
+# this is a trial to pull and push using rstudio 
+
+# PULL
+# step1: file -> new project -> from git -> copy url of your interested repository online
+# step2: select the file from Files in the cloned project folder
 
 
-versionf 
-DDDDD
+# PUSH 
+# 
