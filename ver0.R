a5650e3c3789413049aba9e9c9f0dd608c855e01
gzl <gzlcuhk@gmail.com>
2018-08-01 10:53:53 +0800

    add comment

--- ver0.R
+++ ver0.R
@@ -1,14 +1,22 @@
 # this is a trial to pull, commit and push using rstudio 
 
+
+#------------------------------------------------------------------------
 # PULL
+
+# ORIGINAL: WHEN STARTING A NEW PROJECT
 # step1: file -> new project -> from git -> copy url of your interested repository online
 # step2: select the file from Files in the cloned project folder
 
+# SECONDARY: WHEN CONTINUE TO WORK ON A DOWNLOADED REPOSITORY
+# step1: Tools -> version control -> pull
 
+#-----------------------------------------------------------------------
 # COMMIT
 # step1: Tools -> version control  (control +option +M) -> select adjsuted files
 # step2: commit (add your comment / descruiption of the adjustment)
 
+#-----------------------------------------------------------------------
 # PUSH 
 # step1: Tools -> version control -> push (log in by username and password) 
 #         OR  DIRECTLY SELECT PUSH BUTTON IN THE UPPER-RIGHT CORBERF AFTER COMMIT 
